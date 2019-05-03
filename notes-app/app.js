00bb50f1f27856644af190d277a0dd651804805f
Louie Williford <dustyfingers01257@gmail.com>
2019-05-03 13:38:38 -0600

    learned about yargs and using it to parse command line args

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,47 @@
-const getNotes = require('./notes.js'),
+const chalk = require('chalk'),
+      yargs = require('yargs'),
       validator = require('validator'),
-      chalk = require('chalk');
+      getNotes = require('./notes.js');
+
+// customize yargs version
+yargs.version('1.1.0') ;
+
+// add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note.',
+  handler: () => {
+    console.log('Adding a new note!');
+  }
+});
+
+// remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Delete an existing note.',
+  handler: () => {
+    console.log('Removing a note.');
+  }
+});
+
+// list command
+yargs.command({
+  command: 'list',
+  describe: 'Show list of existing notes.',
+  handler: () => {
+    console.log('Showing list of notes!');
+  }
+});
+
+// read command
+yargs.command({
+  command: 'read',
+  describe: 'Read an individual note.',
+  handler: () => {
+    console.log('Reading a note!');
+  }
+});
 
 
-const msg = getNotes();
-console.log(msg);
-
-console.log(validator.isEmail(msg));
-console.log(validator.isEmail('therealemail@gmail.com'));
-console.log(validator.isURL('https://gmail.com'));
-
-let boldError = chalk.red.bold('Error :(');
-console.log(boldError);
+console.log(process.argv);
+console.log(yargs.argv);
