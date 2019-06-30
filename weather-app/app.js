f5f23aa2e8d242b5d42994fb72335dcbfdc27d0b
dustyfingers <dustyfingers01257@gmail.com>
2019-06-30 17:48:07 -0600

    built out a more robust request to the darksky api

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,14 +2,13 @@ const request = require('request');
 const url = 'https://api.darksky.net/forecast/0476687bab36c26bad5401f69e6879bc/37.8267,-122.4233';
 
 request({ url: url, json: true }, (error, response) => {
-    // 
-    // Print a forcast to the user
-    // 'It is currently 58.55 degrees out. There is a 0% chance of rain.'
-    // 
-    let currentTemp, currentPrecipProb;
+    let currentTemp, currentPrecipProb, dailyHigh, dailyLow;
     currentTemp = response.body.currently.temperature;
     currentPrecipProb = response.body.currently.precipProbability;
-    console.log(`It is currently ${currentTemp} degrees F out. There is a ${currentPrecipProb}% chance of rain.`);
+    dailyHigh = response.body.daily.data[0].temperatureMax;
+    dailyLow = response.body.daily.data[0].temperatureMin;
+    console.log(`It is currently ${currentTemp} degrees F out in Los Angeles, CA. There is a ${currentPrecipProb}% chance of rain.\n`);
+    console.log(`The high for today is ${dailyHigh} and the low is ${dailyLow}.\n`);
 });
 
 
