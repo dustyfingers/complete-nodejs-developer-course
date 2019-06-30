9284e97ee8c1e88eaa873b3ef820148b10356fa7
dustyfingers <dustyfingers01257@gmail.com>
2019-06-30 17:18:37 -0600

    learned about parsing json from apis

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,7 @@
-console.log('Starting...');
+const request = require('request');
+const url = 'https://api.darksky.net/forecast/0476687bab36c26bad5401f69e6879bc/37.8267,-122.4233';
 
-setTimeout(() => {
-    console.log('setTimeOut cb ran!')
-}, 2000);
-
-setTimeout(() => {
-    console.log('second setTimeOut cb ran!')
-}, 0);
-
-console.log('Finished!');
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.currently);
+});
