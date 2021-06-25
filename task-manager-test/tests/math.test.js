af481d5df64e16db75b7b17fee41e8db29dc6715
lou <dustyfingers01257@gmail.com>
2021-06-25 11:53:53 -0600

    added task manager test project

---
+++ task-manager-test/tests/math.test.js
@@ -0,0 +1,22 @@
+const { calculateTip } = require('../src/math');
+
+test('should calculate total with tip', () => {
+    const total = calculateTip(10, .3);
+    expect(total).toBe(13);
+});
+
+test('should calculate total with default tip', () => {
+    const total = calculateTip(10);
+    expect(total).toBe(12);
+});
+
+test();
+
+// * Why test??
+// - Saves time
+// - Creates reliable software
+// - Gives flexibility to developers
+// - Refactoring
+// - Collaborating
+// - Profiling
+// - Peace of mind
