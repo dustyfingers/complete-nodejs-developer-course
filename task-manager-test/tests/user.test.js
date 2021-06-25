33037e11ec646f8949713869b495b93e96a9a41b
lou <dustyfingers01257@gmail.com>
2021-06-25 12:34:45 -0600

    wrote simple sign up user successful test case

--- task-manager-test/tests/user.test.js
+++ task-manager-test/tests/user.test.js
@@ -1,4 +1,10 @@
 const request = require('supertest')
 const app = require('../src/app')
 
-
+test('should sign up a new user', async () => {
+    await request(app).post('/users').send({
+        name: "Andrew",
+        email: "andrew123@example.com",
+        password: "mypass777!@"
+    }).expect(201)
+})
