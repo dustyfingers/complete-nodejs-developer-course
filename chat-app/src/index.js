efb4e87b6809086beec860e4166887a868d30ffc
lou <dustyfingers01257@gmail.com>
2021-07-03 11:52:52 -0600

    set up websocket support and added build scripts

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,7 +1,11 @@
 const path  = require('path')
 const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 5000
 const publicDir = path.join(__dirname, '../public')
