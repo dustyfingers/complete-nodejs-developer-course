e21b400d67000c8fe4b388116f6ca1581936389d
lou <dustyfingers01257@gmail.com>
2021-07-03 12:14:54 -0600

    set up socket io on client side

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,15 +1,22 @@
-const path  = require('path')
-const express = require('express')
-const http = require('http')
-const socketio = require('socket.io')
+const express = require('express');
+const path = require('path');
+const socketio = require('socket.io');
+ 
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const port = process.env.PORT || 4446;
+const publicDirPath = path.join(__dirname, '../public');
 
-const port = process.env.PORT || 5000
-const publicDir = path.join(__dirname, '../public')
+const app = express();
+app.use(express.static(publicDirPath));
+ 
+const server = app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+});
+ 
+socketio.listen(server);
+const io = socketio(server);
 
-app.use(express.static(publicDir))
-
-app.listen(port, () => console.log(`Server started on port ${port}`))
+// Set up some message when socket.io gets 'connectoion' event
+io.on('connection', () => {
+    console.log('New websocket connection');
+});
